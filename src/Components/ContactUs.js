1e25e6f0c5c74fd13042618358e3f3b1776cfe87
Shyamal <shivakumarsk5308@gmail.com>
2024-05-30 00:19:48 +0530

    animation updated

--- src/Components/ContactUs.js
+++ src/Components/ContactUs.js
@@ -5,59 +5,27 @@ import { IoMailOutline } from "react-icons/io5";
 import emailjs from "@emailjs/browser";
 
 const ContactUs = () => {
-  //   const [inputClicked, setInputClicked] = useState(false);
-  //   const [userName, setUserName] = useState("");
-  const [userEmail, setUserEmail] = useState("");
-  //   const [userMessage, setUserMessage] = useState("");
-  const [isEmail, setIsEmail] = useState(false);
-
-  //   const handleChangeclick = () => {
-  //     setInputClicked(true);
-  //   };
-  //   const isvalidemail = () => {
-  //     setIsEmail(false);
-  //   };
-
-  const validateEmail = () => {
-    const isemail =
-      userEmail.toLowerCase().endsWith("gmail.com") &&
-      userEmail.toLowerCase().slice(-1) === "m";
-    setIsEmail(isemail);
-    console.log(isemail);
-  };
-
-  const validemail = (e) => {
-    setUserEmail(e.target.value);
-
-    // setIsEmail(validateEmail());
-    validateEmail();
-    // console.log(userEmail.toLowerCase().endsWith("gmail.com"));
-    // console.log(isEmail);
-  };
+  const form = useRef();
 
   const sentEmail = (e) => {
     e.preventDefault();
 
-    // console.log(form.current);
-    console.log("hi");
-
-    // emailjs
-    //   .sendForm("service_zx33b5i", "template_tln3229", form.current, {
-    //     publicKey: "lh5SpTWnbeMLuG8a1",
-    //   })
-    //   .then(
-    //     () => {
-    //       console.log("SUCCESS!");
-    //     },
-    //     (error) => {
-    //       console.log("FAILED...", error.text);
-    //     }
-    //   );
+    emailjs
+      .sendForm("service_zx33b5i", "template_tln3229", form.current, {
+        publicKey: "lh5SpTWnbeMLuG8a1",
+      })
+      .then(
+        () => {
+          console.log("SUCCESS!");
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
 
     // e.target.reset();
   };
 
-  useEffect(() => {}, [isEmail]);
   return (
     <div className="bg-gradient-to-b from-black to-gray-800 p-5">
       <div className="flex flex-col justify-centerr items-center md:px-20">
@@ -113,7 +81,11 @@ const ContactUs = () => {
           <div className="flex  md:w-1/2  md:mt-8 md:p-9">
             <div className="flex flex-col bg-white w-full border border-black items-start p-10">
               <h2 className="text-3xl my-3 font-bold">Send Message</h2>
-              <form onSubmit={sentEmail} className="flex flex-col w-full my-2">
+              <form
+                ref={form}
+                onSubmit={sentEmail}
+                className="flex flex-col w-full my-2"
+              >
                 <label
                   htmlFor="fullname"
                   className="text-gray-600 font-bold my-1"
@@ -122,11 +94,9 @@ const ContactUs = () => {
                 </label>
                 <input
                   type="text"
-                  //   onChange={(e) => setUserName(e.target.value)}
                   name="user_name"
                   id="fullname"
                   className="border-b-2 my-2 border-gray-900 focus:outline-none"
-                  //   onClick={handleChangeclick}
                   required
                 />
                 <label htmlFor="email" className="text-gray-600 font-bold">
@@ -135,18 +105,10 @@ const ContactUs = () => {
                 <input
                   type="email"
                   name="user_email"
-                  value={userEmail}
-                  onChange={validemail}
-                  //   onFocus={isvalidemail}
                   id="email"
                   className="border-b-2 my-2 border-gray-900 focus:outline-none"
                   required
                 />
-                {/* {isEmail ? (
-                  ""
-                ) : (
-                  <p className="text-red-500">Please Input valid email</p>
-                )} */}
 
                 <label htmlFor="message" className="text-gray-600 font-bold">
                   Message
@@ -154,7 +116,6 @@ const ContactUs = () => {
                 <textarea
                   name="message"
                   id="message"
-                  //   onChange={(e) => setUserMessage(e.target.value)}
                   placeholder="type your message here..."
                   className="border-b-2 my-2 border-gray-900 focus:outline-none"
                   required
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,18 +1,42 @@
 import React from "react";
 import myimage from "../assets/myimage.avif";
+import { Typewriter, useTypewriter, Cursor } from "react-simple-typewriter";
 // import { MdOutlineFileDownload } from "react-icons/md";
 
 const Home = () => {
+  // const { words } = useTypewriter({
+  //   words: [
+  //     "Frontend Developer",
+  //     "React Developer",
+  //     "Javascript Developer",
+  //     "Software Developer",
+  //   ],
+  //   loop: {},
+  // });
+
+  // console.log(words);
   return (
-    <div className="h-screen w-full bg-gradient-to-b from-black to-gray-800">
-      <div className="flex flex-col md:flex-row md:py-8 justify-center mx-w-screen-lg mx-auto items-center px-4">
+    <div className="h-screen w-full flex items-center bg-gradient-to-b from-black to-gray-800">
+      <div className="flex flex-col md:flex-row justify-center mx-w-screen-lg mx-auto items-center px-4">
         <div className="flex flex-col justify-center h-full">
           <h4 className="text-white text-xl ">Hello</h4>
-          <h1 className="text-amber-300 my-2 max-w-md text-4xl sm:text-3xl font-bold">
+
+          <h1 className="text-amber-300 my-2 max-w-md text-4xl md:text-7xl font-bold">
             I'm Shyamal Bhattacharya
           </h1>
-          <h2 className="text-white font-bold my-2 text-2xl">
-            Frontend Developer
+          <h2 className="text-cyan-500 font-bold my-2 text-2xl">
+            <Typewriter
+              words={[
+                "Frontend Developer",
+                "React Developer",
+                "Javascript Developer",
+                "Software Developer",
+              ]}
+              loop={Infinity}
+            />
+            <span>
+              <Cursor />
+            </span>
           </h2>
           <p className="text-white  my-2 max-w-md">
             An aspiring frontend developer, I bring fresh perspectives and
@@ -20,13 +44,8 @@ const Home = () => {
             for design and a knack for problem-solving, I strive to create
             impactful digital solutions that leave a lasting impression.
           </p>
-          {/* <div className="flex justify-end">
-            <button className="flex items-center rounded-xl mt-4 px-5 p-2 bg-black text-white">
-              Resume <MdOutlineFileDownload className="text-white ml-2" />
-            </button>
-          </div> */}
         </div>
-        <div className="md:px-20 my-8">
+        <div className="md:px-20 ">
           <img
             className="rounded-lg"
             width={300}
